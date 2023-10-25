be394ed75ca810368be4da95c51240d5df0aaeb7
Garsche <garscheroble@gmail.com>
2023-10-25 09:44:45 -0500

    Note Sound Changes To Music

--- Assets/Scripts/Note.cs
+++ Assets/Scripts/Note.cs
@@ -6,7 +6,6 @@ public class Note : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        FMODUnity.RuntimeManager.PlayOneShot("event:/Note G");
         Destroy(gameObject);
     }
 }
