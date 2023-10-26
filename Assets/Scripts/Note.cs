27d6ac788f036ec1d746d05015f43e4070f901bb
Garsche <garscheroble@gmail.com>
2023-10-25 20:28:12 -0500

    Updated Note Playback (fixed tail)

--- Assets/Scripts/Note.cs
+++ Assets/Scripts/Note.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class Note : MonoBehaviour
 {
+    //Declaring FMOD variable.
+    private FMOD.Studio.EventInstance melody;
     private void OnTriggerEnter(Collider other)
     {
+        //Setting FMOD variable to an FMOD event. Will start and immediately release the event.
+        melody = FMODUnity.RuntimeManager.CreateInstance("event:/Notes/Note Collect");
+        melody.start();
+        melody.release();
+        
         Destroy(gameObject);
     }
 }
