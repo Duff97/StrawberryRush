21a9b98a2768c888db3dc71bbb996380599714d3
Duff97 <felduf12@gmail.com>
2023-11-06 21:44:03 -0500

    Different note inputs

--- Assets/Scripts/Notes/Note.cs
+++ Assets/Scripts/Notes/Note.cs
@@ -1,13 +1,21 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Note : MonoBehaviour
 {
     private enum NoteValues { A4, A5, Ab4, Ab5, B4, B5, Bb4, Bb5, C4, C5, C6, D5, Eb4, Eb5, F5, G4, G5, Gb4, Gb5 };
+    private enum KeyValues { Z, X, C }
+
     [SerializeField] private NoteValues noteValue;
-    [SerializeField] private KeyCode collectKey = KeyCode.E;
+    [SerializeField] private KeyValues keyValue;
+    [SerializeField] private Material[] materials;
+    [SerializeField] private Renderer noteRenderer;
+    [SerializeField] private TMP_Text textField;
+
+    private KeyCode collectKey;
 
     public static event Action<string> OnNoteCollected;
 
@@ -18,6 +26,8 @@ public class Note : MonoBehaviour
     {
         GameManager.OnGameStarted += HandleGameStart;
         effect = GetComponent<NoteEffect>();
+        collectKey = KeyToKeyCode(keyValue.ToString());
+        SetNoteMaterial();
     }
 
     private void Update()
@@ -53,6 +63,23 @@ public class Note : MonoBehaviour
     private void HandleGameStart()
     {
         gameObject.SetActive(true);
+    }
+
+    private KeyCode KeyToKeyCode(string key)
+    {
+        switch (key)
+        {
+            case "Z": return KeyCode.Z;
+            case "X": return KeyCode.X;
+            case "C": return KeyCode.C;
+            default: return KeyCode.None;
+        }
+    }
+
+    private void SetNoteMaterial()
+    {
+        noteRenderer.material = materials[(int)keyValue];
+        textField.text = keyValue.ToString();
     }
 
     private void OnDestroy()
