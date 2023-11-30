8fdbe96720f1af9c6153ce89f3fc997490f0830c
Duff97 <felduf12@gmail.com>
2023-11-29 19:08:42 -0500

    Suicide functionality, footstep after death fix

--- Assets/Scripts/Player/PlayerDeath.cs
+++ Assets/Scripts/Player/PlayerDeath.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 public class PlayerDeath : MonoBehaviour
 {
     [SerializeField] private ParticleSystem deathParticles;
+    [SerializeField] private KeyCode suicideKey = KeyCode.R;
+
+    private bool suicideActive = false;
 
     public static event Action OnPlayerDeath;
 
@@ -13,17 +16,42 @@ public class PlayerDeath : MonoBehaviour
     {
         WallDetector.OnWallDetected += TriggerDeath;
         FallDetector.OnFallDetected += TriggerDeath;
+        GameManager.OnGameStarted += ActivateSuicide;
+        FinishLineDetector.OnFinishLinePassed += DeactivateSuicide;
+    }
+
+    private void Update()
+    {
+        if (!suicideActive) return;
+
+        if (Input.GetKeyDown(suicideKey))
+        {
+            TriggerDeath();
+        }
     }
     private void TriggerDeath()
     {
         deathParticles.Play();
         OnPlayerDeath?.Invoke();
+        DeactivateSuicide();
+    }
+
+    private void ActivateSuicide()
+    {
+        suicideActive = true;
+    }
+
+    private void DeactivateSuicide()
+    {
+        suicideActive = false;
     }
 
     private void OnDestroy()
     {
         WallDetector.OnWallDetected -= TriggerDeath;
         FallDetector.OnFallDetected -= TriggerDeath;
+        GameManager.OnGameStarted -= ActivateSuicide;
+        FinishLineDetector.OnFinishLinePassed -= DeactivateSuicide;
     }
 
 
--- Assets/Scripts/Player/PlayerFootstep.cs
+++ Assets/Scripts/Player/PlayerFootstep.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerFootstep : MonoBehaviour
@@ -9,9 +10,35 @@ public class PlayerFootstep : MonoBehaviour
 
     public static event Action OnFootstep;
 
+    private bool active = false;
+
+    private void Start()
+    {
+        GameManager.OnGameStarted += Activate;
+        PlayerDeath.OnPlayerDeath += Deactivate;
+    }
+
     public void TriggerFootstep()
     {
+        if (!active) return;
+
         OnFootstep?.Invoke();
         footstepParticle.Play();
+    }
+
+    private void Activate()
+    {
+        active = true;
+    }
+
+    private void Deactivate()
+    {
+        active = false;
+    }
+
+    private void OnDestroy()
+    {
+        GameManager.OnGameStarted -= Activate;
+        PlayerDeath.OnPlayerDeath -= Deactivate;
     }
 } 
